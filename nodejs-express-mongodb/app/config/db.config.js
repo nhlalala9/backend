044f49263328dca96ffc238ce2f8e0738864abba
Nhlalala <nhlalala60@gmail.com>
2023-05-02 12:16:43 +0200

    connecting

---
+++ nodejs-express-mongodb/app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    url: "mongodb+srv://Nhlalala:" + encodeURIComponent("Nhlalala#9") + "@cluster0.xlwb2yt.mongodb.net/test"
+  };
+
+  
+  
--- nodejs-express-mongodb/server.js
+++ nodejs-express-mongodb/server.js
@@ -2,12 +2,30 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+app.use(cors(corsOptions));
+
+// connecting the database
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 var corsOptions = {
-  origin: "http://localhost:8080"
+  origin: "http://localhost:8081"
 };
 
-app.use(cors(corsOptions));
+
+
+
 
 // parse requests of content-type - application/json
 app.use(express.json());
